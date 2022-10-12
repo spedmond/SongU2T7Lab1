2071b0d3288dee9046ea1cd11b0adc4604fbd505
unknown <BT_3W2_25@BKTECH.LOCAL>
2022-10-12 12:27:38 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        String today = "FRIDAY!";
+        String x = today.substring(3, 7).substring(0, 3).substring(1, 3);
+        System.out.println(x);
+    }
+}
